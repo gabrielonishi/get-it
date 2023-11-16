a994762ac8aa3d9e238aa105a29c2548cff4a980
gabrielhso <gabriel.onishi@outlook.com>
2023-11-16 20:25:20 -0300

    Adiciona template inicial do site

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Olá mundo! Este é o app notes de Tecnologias Web do Insper.")
+    return render(request, 'notes/index.html')
